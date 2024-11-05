b2f04dfedba43578473da126224967357a8f04c2
Som Chandra <85082756+Somchandra17@users.noreply.github.com>
2024-11-05 11:14:51 +0530

    Create paypalConfig.js

---
+++ paypalConfig.js
@@ -0,0 +1,8 @@
+// Mock PayPal Client ID and Secret
+const paypalConfig = {
+  mode: 'sandbox',
+  client_id: 'AeDemoPayPalClientID1234567890abcdef',
+  client_secret: 'EDemoPayPalClientSecret_ABCDEF1234567890',
+};
+
+module.exports = paypalConfig;
