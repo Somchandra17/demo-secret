9409a227d8a2c06c7684eb464b2e0f8ab4ee0b54
Som Chandra <85082756+Somchandra17@users.noreply.github.com>
2024-11-05 11:15:21 +0530

    Create azureConfig.js

---
+++ azureConfig.js
@@ -0,0 +1,7 @@
+// Mock Azure Storage Account Credentials
+const azureConfig = {
+  accountName: 'demostorageaccount',
+  accountKey: 'demoAccountKey1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ==',
+};
+
+module.exports = azureConfig;
