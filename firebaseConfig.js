2f1238a410ea64d7e73aebcaf911f0b1c7ea4732
Som Chandra <85082756+Somchandra17@users.noreply.github.com>
2024-11-05 11:14:01 +0530

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// Mock Firebase Configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDemoApiKey123456789',
+  authDomain: 'demo-project.firebaseapp.com',
+  databaseURL: 'https://demo-project.firebaseio.com',
+  projectId: 'demo-project',
+  storageBucket: 'demo-project.appspot.com',
+  messagingSenderId: '123456789012',
+  appId: '1:123456789012:web:demoAppId1234567890abcdef',
+};
+
+module.exports = firebaseConfig;
