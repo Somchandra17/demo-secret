c1cbfd59f6977369656a31093a777d0294d22860
Som Chandra <85082756+Somchandra17@users.noreply.github.com>
2024-11-05 11:14:19 +0530

    Create googleOAuth.js

---
+++ googleOAuth.js
@@ -0,0 +1,12 @@
+// Mock Google OAuth 2.0 Client ID and Client Secret
+const CLIENT_ID = '123456789012-abcdefghijklmnopqrstuvwxyz.apps.googleusercontent.com';
+const CLIENT_SECRET = 'DemoGoogleClientSecret_ABCDEF123456';
+
+// Mock Refresh Token
+const REFRESH_TOKEN = '1//0demoRefreshTokenABCDEF1234567890ghijklmnop';
+
+module.exports = {
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REFRESH_TOKEN,
+};
