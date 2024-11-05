962667d6b9be3af58d322fd56651881d450db311
Som Chandra <85082756+Somchandra17@users.noreply.github.com>
2024-11-05 11:16:18 +0530

    Create twilioConfig.js

---
+++ twilioConfig.js
@@ -0,0 +1,8 @@
+// Mock Twilio Credentials
+const accountSid = 'ACdemoTwilioAccountSid1234567890abcdef';
+const authToken = 'demoTwilioAuthToken_abcdef1234567890';
+
+module.exports = {
+  accountSid,
+  authToken,
+};
